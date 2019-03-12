49cdd934be59d98d293f52db6cf1236804425a4b
DESKTOP-4UDOJS8\Orell <orell.endres@stud.hslu.ch>
2019-03-12 13:17:36 +0100

    Storage, Up/Download Image, storing values in to mongoDB with helperfunction

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ let http = require('http').Server(app);
 let path = require('path');
 let fs = require('fs');
 
+
 const mongodb = require('mongodb');
 const nconf = require('nconf');
+const Multer = require('multer');
+const bodyParser = require('body-parser');
+const { Storage } = require('@google-cloud/storage');
+const format = require('util').format;
+
+require("dotenv").config();
 
 // Read in keys and secrets. Using nconf use can set secrets via
 // environment variables, command-line arguments, or a keys.json file.
@@ -19,44 +26,97 @@ const pass = nconf.get('mongoPass');
 const host = nconf.get('mongoHost');
 const port = nconf.get('mongoPort');
 
+//Initiate Storage client
+const storage = new Storage();
+//bucket
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+
 
 
 var MongoClient = require('mongodb').MongoClient;
 
-var uri = `mongodb://${user}:${pass}@instantcluster-shard-00-00-z05ud.gcp.mongodb.net:27017,instantcluster-shard-00-01-z05ud.gcp.mongodb.net:27017,instantcluster-shard-00-02-z05ud.gcp.mongodb.net:27017/test?ssl=true&replicaSet=InstantCluster-shard-0&authSource=admin&retryWrites=true`;
-
-mongodb.MongoClient.connect(uri, { useNewUrlParser: false }, (err, client) => {
-     if (err) {
-       throw err;
-     }
-
-    const db = client.db(nconf.get("mongoDatabase"));
-    const photoscollection = db.collection("Photos");
-
-    let object = {
-      name: "test",
-      date: "1.1.1111",
-      test: "test"
-    }
-
-    photoscollection.insertOne(object, (err, result) =>{
-      if (err){
-        throw err;
-      }
-      console.log(result);
-    })
-
-})
-
-
 // serve the index.html as starting page
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
 });
 
 // serve all files in dist
 app.use(express.static('dist'));
 
-http.listen(process.env.PORT || 8080, function(){
-    console.log(`listening on *: ${http.address().port}`);
+//Image Upload Handling
+
+//Makes Requests readable for server
+app.use(bodyParser.json());
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024, // no larger than 10mb
+  },
 });
+
+//Server Listener
+
+app.post("/uploads", multer.single("file"),(req,res,next)=>{
+  console.log("uploading");
+  if(!req.file) {
+    console.log("No File Detected");
+    res.status(400).send("No File Uploaded");
+    return;
+  }
+
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+  })
+
+  blobStream.on("error", err=>{
+    console.log(err),
+    next(err)
+  });
+
+  blobStream.on("finish", () =>{
+    const publicURL = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+    res.status(200).send(publicURL);
+    var objectToBeStored = {
+      name: blob.name,
+      date: new Date(),
+      url: publicURL
+    }
+    storeIntoMongoDB(objectToBeStored,"photos");
+  })
+
+  blobStream.end(req.file.buffer);
+
+})
+
+
+
+http.listen(process.env.PORT || 8080, function(){
+  console.log(`listening on *: ${http.address().port}`);
+});
+
+
+
+function storeIntoMongoDB(object, collectionName) {
+
+  var uri = `mongodb://${user}:${pass}@instantcluster-shard-00-00-z05ud.gcp.mongodb.net:27017,instantcluster-shard-00-01-z05ud.gcp.mongodb.net:27017,instantcluster-shard-00-02-z05ud.gcp.mongodb.net:27017/test?ssl=true&replicaSet=InstantCluster-shard-0&authSource=admin&retryWrites=true`;
+
+  mongodb.MongoClient.connect(uri, { useNewUrlParser: false }, (err, client) => {
+    if (err) {
+      throw err;
+    }
+
+    const db = client.db(nconf.get("mongoDatabase"));
+    const collection = db.collection(collectionName);
+
+    collection.insertOne(object, (err, result) =>{
+      if (err){
+        throw err;
+      }
+      client.close();
+
+    })
+
+  })
+
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+<input
+  type="file"
+  id="imageupload"
+  name="imageupload"
+  accept="image/png, image/jpg"
+  ref="fileinput"
+  >
+<button @click="upload">Upload</button>
+<img  :src="cloudimage">
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      files: null,
+      cloudimage: ""
+    }
+  },
+  methods: {
+    upload: function(){
+      let data = new FormData();
+      data.append("file", this.$refs.fileinput.files[0]);
+
+      fetch("/uploads", {
+        method: "POST",
+        body: data
+      })
+      .then(response => {return response.text();})
+      .then(mytext => {
+        this.cloudimage = mytext;
+      })
+    }
   }
 };
 </script>
